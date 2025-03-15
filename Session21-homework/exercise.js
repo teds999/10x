313f93f7151cfb3a45b9f95ef1c0d635bb438636
KOT <tedoturmanidze12@gmail.com>
2025-03-15 21:11:26 +0300

    homework done succesfully

---
+++ Session21-homework/exercise.js
@@ -0,0 +1,321 @@
+// WRITING IN .js FILE TYPE , CHECKING IN TERMINAL. GL HOMEWORKHUNTER ( BY : TEDO TURMANIDZE)
+
+
+
+
+    // EXERCISE 1
+    // let gandalfPower = 15;
+    // let dumbledorePower=20;
+    
+    // gandalfPower += 5;
+    // dumbledorePower -= 2;
+    
+    // console.log(`new power of gandalf : ${gandalfPower}`);
+    // console.log(`new power of dumbledore : ${dumbledorePower}`);
+    
+    // if( gandalfPower > dumbledorePower){
+    //   console.log("gandalf dominates");
+    // } else  if ( gandalfPower < dumbledorePower) {
+    //   console.log("dumbledore dominates");  
+    // } else {
+    //   console.log("their powers are equal. its tie")
+    // }
+    
+
+
+
+
+
+
+    // EXERCISE 2
+    // let goldCoin = 500;
+    // let treasureMap = "5x5 island";
+    // let cursedAmulet = true;
+
+    // console.log(typeof goldCoin);
+    // console.log(typeof treasureMap);
+    // console.log(typeof  cursedAmulet);
+    
+
+
+
+
+
+
+
+    // EXERCISE 3
+    // let healtPotion = 15;
+    // let manaPotion= 11;
+
+    // let totalCost = (healtPotion * 3) + (manaPotion * 2);
+
+    // if (totalCost > 50){
+    //   console.log(`you got  discount and instead of ${totalCost} you get all items 2x cheaper , new price > ${totalCost / 2}`);
+    // } else {
+    //   console.log(`your total cost is ${totalCost}`);
+    // }
+
+
+
+
+
+    
+    // EXERCISE 4
+    // let atackPower = 12;
+    // let armor = "mcenareuloba";
+    // let shield = true ;
+
+    // console.log(`papa is ready for war with ${atackPower} atackpower , with greatest armor of all time ${armor} , and ofcourse with shield (shield on hand = ${shield})`);
+
+
+
+
+
+
+
+    // EXERCISE 5
+    // let cakePrice = 3;
+    // let breadPrice=5;
+    // let dailySales = cakePrice + breadPrice;
+
+    // console.log(`dayle earning : ${dailySales}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // EXERCISE 6
+    // let spaceshipCapacity = 114;
+    // let spaceshipFuelLevel = 93;
+
+    // if(spaceshipFuelLevel > (spaceshipCapacity * 80 / 100)){
+    //     console.log("great job mr.katistava");
+    // } else {
+    //     console.log("gadi daazapravke jigaro shena ha");
+    // }
+
+
+
+
+
+
+
+
+
+
+
+
+    //      EXERCISE 7
+    // let currentTime = new Date().getFullYear();
+    // let yearsSkipped = 5 ;
+    // let newTime = currentTime + yearsSkipped ;
+
+    // console.log(`we are in ${newTime} year sir`);
+
+
+
+
+
+
+
+
+        // EXERCISE 8
+
+        // let spellCross = 32;
+        // let spellKick = 42424;
+        // let spellSlap = 44;
+        
+        // if (spellCross > spellKick && spellCross > spellSlap) {   
+        //     console.log("strongest spell is Cross");
+        // } else if (spellKick > spellCross && spellKick > spellSlap) {
+        //     console.log("strongest spell is Kick");
+        // } else {
+        //     console.log("strongest spell is Slap");
+        // }
+
+
+
+
+
+
+
+
+
+        //     EXERCISE 9 (made it easier with just 2 variables dont sue )
+        // let kichenHasGhost = false ;
+        // let livingroomHasGhost = true;
+
+        // if (kichenHasGhost === false && livingroomHasGhost === false) {
+        //     console.log("house is free from ghost !");
+        // } else {
+        //     console.log("oops , i hear somebody in the house");
+        // }
+
+
+
+
+
+
+
+
+
+        //     EXERCISE 10
+        // let boltBatteryCapacity = 100;
+        // let boltBattery = 33;
+
+        // if(boltBattery > (boltBatteryCapacity * 30 /100)){
+        //     console.log("gamdzle bolt gaika");
+        // }else {
+        //     console.log("maita damtene aba jima");
+        // }
+
+
+
+
+
+
+
+
+        //     EXERCISE 11 
+        // let dragonsHealth = 320 ;
+        // let knightAttackDmg = 75 ;
+        // dragonsHealth -= knightAttackDmg ;
+        // console.log(`dragons health after knights attack = ${dragonsHealth}`);
+
+
+
+
+
+
+
+
+
+        // EXERCISE 12
+        // let speedlimit = 110;
+        // let carSpeed = 80;
+        // carSpeed += 12 ;
+
+        // if(carSpeed > speedlimit){
+        //     console.log(`slow down brother mother is waiting for you . ( currect car speed is ${carSpeed})`);
+        // }else {
+        //     console.log("you such a obey booy");
+        // }
+
+
+
+
+
+
+
+        //      EXERCISE 13
+        // let heroStrength = 95;
+        // let villainStrength = 90;
+
+        // if (heroStrength > villainStrength + 10) {
+        //     console.log("Valiant triumphs decisively! Malakor is utterly outmatched!");
+        // } else if (villainStrength > heroStrength + 10) {
+        //     console.log("Malakor's power is overwhelming! Valiant is soundly defeated!");
+        // } else if (heroStrength === villainStrength) {
+        //     console.log("It's a strength stalemate! Neither Valiant nor Malakor can overpower the other.");
+        // } else if (heroStrength > villainStrength) {
+        //     console.log("Valiant barely edges out! A close call victory against Malakor!");
+        // } else {
+        //     console.log("Malakor narrowly prevails! A hard-fought win over Valiant!");
+        // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //         EXERCISE 14
+        // let hasTreasureMap = true;  
+        // let foundAncientKey = false;  
+        // let solvedRiddleScroll = true;  
+        // let hasCompass = true;  
+        // let isFullMoon = false;  
+
+
+        // let canFindTreasure = hasTreasureMap && (foundAncientKey || (solvedRiddleScroll && hasCompass));
+
+
+        // if (canFindTreasure) {
+        //     console.log("Success! Alex can find the legendary treasure!");
+        // } else {
+        //     console.log("Alas, Alex is missing crucial clues and cannot find the treasure yet.");
+        // }
+
+
+
+
+
+
+
+
+
+
+            // EXERCISE 15  
+
+            // const creamCone = 4 ;
+            // const creamSundae = 8 ;
+            // let conesSoldToday = 250;
+            // let sundaesSoldToday = 120;
+
+            // let totalEarnings = (creamCone*conesSoldToday) + (creamSundae*sundaesSoldToday) ;
+
+            // if (totalEarnings >= 1000) {
+            //     console.log(`total earnings are ${totalEarnings} , because of that discount of 5% applies and earnings are ${totalEarnings*0.95}`)
+            // } else {
+            //     console.log(`total earnings are ${totalEarnings}`)
+            // }
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
