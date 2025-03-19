fa2ddb3cba90230216365ed6648f4c1cbb9e0ff5
KOT <tedoturmanidze12@gmail.com>
2025-03-19 21:40:32 +0300

    started to do homeworks and got motivated streak 2~

---
+++ Session22-functions.js
@@ -0,0 +1,37 @@
+// WRITING IN .js FILE TYPE , CHECKING IN TERMINAL. GL HOMEWORKHUNTER ( BY : TEDO TURMANIDZE)
+
+
+// EXERCISE 1
+// function compareAttack(pokemon1,atack1,pokmen2,atack2){
+//   if(atack1 > atack2){
+//     console.log(`${pokemon1} is stronger`);
+//   }else if (atack2 > atack1){
+//     console.log(`${pokmen2} is stronger`);
+//   } else {
+//     console.log("its tie bro they are equall")
+//   }
+
+// }
+// compareAttack("Pikachu", 515, "Charizard", 515)
+
+
+
+
+
+
+
+// EXERCISE 2 
+// function castSpell(spell,wizard){
+//   console.log(`${wizard} casts ${spell}`);
+// }
+
+// castSpell("Hermione","Expelliarmus");
+
+
+
+
+
+
+
+
+
