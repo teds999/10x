d8f40426f58bb78b73a2d66b1edebe27fc444f2e
KOT <tedoturmanidze12@gmail.com>
2025-04-11 21:55:47 +0300

    renewed learning

--- Session23-conditions.js
+++ Session23-conditions.js
@@ -2,22 +2,38 @@
 
 
 
-// CC#1
-let markMass = 178;
-let markHeight = 1.69;
+// CC#1-2
+// let markMass = 178;
+// let markHeight = 1.69;
 
-let johnMass = 92 ;
-let johnHeight = 1.95;
+// let johnMass = 92 ;
+// let johnHeight = 1.95;
 
-let markBmi = markMass / (markHeight*2);
-let johnBmi = johnMass / (johnHeight*2);
-
-
-console.log(`Marks height is ${markHeight}m and mass is ${markMass}kg , Johns height is ${johnHeight}m and mass is ${johnMass}kg`);
+// let markBmi = markMass / (markHeight*2);
+// let johnBmi = johnMass / (johnHeight*2);
 
 
-if (markBmi>johnBmi){
-  console.log("Mark's BMI is higher than John's");
-}else{
-  console.log("John's BMI is higher than Mark's");
-}
+// console.log(`Marks height is ${markHeight}m and mass is ${markMass}kg , Johns height is ${johnHeight}m and mass is ${johnMass}kg`);
+
+
+// if (markBmi>johnBmi){
+//   console.log("Mark's BMI is higher than John's");
+// }else{
+//   console.log("John's BMI is higher than Mark's");
+// }
+
+
+
+
+
+// CC#4
+
+// let orderAmount = 51
+// let tipCounter = 50 < orderAmount && orderAmount < 300 ? 15 : 20
+// let tip = orderAmount * tipCounter / 100
+
+// console.log ( `Your total is = ${orderAmount}$ + ${tip}$  (${orderAmount+tip})`) 
+
+
+
+
